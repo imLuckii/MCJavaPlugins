671fb98401c5c34076e882c7e328b2de02aa52d4
imlucky <167329163+imLuckii@users.noreply.github.com>
2024-10-17 22:04:59 +0000

    - A plugin to dupe shulkers when TNT explodes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# JavaMCDev
+### A repository of plugins I made for fun or specific situations. I don't make Spigot plugins, so these might be bad or just not work. If you need to use any of them, do it at your own risk. Some of the plugins were made for my friend.
+
+I'll keep adding any Java plugin I make here.
---
+++ TNTShulkerDupe/src/main/java/com/lucky/tntdupe/TntShulkerDupe.java
@@ -0,0 +1,47 @@
+package com.lucky.tntdupe;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Server;
+import org.bukkit.block.Block;
+import org.bukkit.block.ShulkerBox;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BlockStateMeta;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.List;
+
+public class TntShulkerDupe extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(this, this);
+    }
+
+    @Override
+    public void onDisable() {
+    }
+
+    @EventHandler
+    public void onTntExplode(EntityExplodeEvent event) {
+        List<Block> affectedBlocks = event.blockList();
+
+        for (Block block : affectedBlocks) {
+            if (block.getState() instanceof ShulkerBox) {
+                ShulkerBox shulkerBox = (ShulkerBox) block.getState();
+
+                ItemStack shulkerBoxItem = new ItemStack(block.getType());
+
+                BlockStateMeta blockStateMeta = (BlockStateMeta) shulkerBoxItem.getItemMeta();
+
+                blockStateMeta.setBlockState(shulkerBox);
+                shulkerBoxItem.setItemMeta(blockStateMeta);
+
+                block.getWorld().dropItemNaturally(block.getLocation(), shulkerBoxItem);
+
+            }
+        }
+    }
+}
